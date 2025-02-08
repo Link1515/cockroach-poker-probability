ab4ce3a893a46de3af1605f5543d0f340467151d
Link1515 <h26281660@gmail.com>
2025-02-08 15:30:26 +0800

    feat: add ModifyCountModal

---
+++ src/components/ModifyCountModal.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { VueFinalModal } from 'vue-final-modal';
+</script>
+
+<template>
+  <VueFinalModal
+    class="flex justify-center items-center"
+    content-class="flex flex-col max-w-xl mx-4 p-4 bg-gray-900 border-gray-700 rounded-lg space-y-2"
+  >
+    <slot />
+  </VueFinalModal>
+</template>
