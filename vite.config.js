a8d27eaaade8fdd0cc02a2e1fcb59a34543880d2
Link1515 <h26281660@gmail.com>
2025-02-09 13:39:03 +0800

    ci: configure github action for github page deployment

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,19 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite';
 
+const projectName = 'cockroach-poker-probability';
+
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), tailwindcss()],
-  resolve: {
-    alias: {
-      '~': path.resolve(__dirname, 'src')
+export default defineConfig(({ mode }) => {
+  const base = mode === 'production' ? `/${projectName}/` : '/';
+
+  return {
+    base,
+    plugins: [vue(), tailwindcss()],
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, 'src')
+      }
     }
-  }
+  };
 });
